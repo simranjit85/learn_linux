57332d9d2c6c88937623cd0d781bbe173a06505a
simranjit85 <ginnisim.ahluwalia85@gmail.com>
2018-03-06 20:16:38 -0600

    Add files via upload

---
+++ buff_assignment.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+    int flag=0;
+    char password[5]="1234";
+    int money_value=600;
+    int choose_value=0;
+    int amount_deposited=0;
+    int amount_withdrawn=0;
+    if(strcmp(password,argv[1])==0){
+        printf("Correct password!\n");
+        flag=1;
+    }
+    else{
+        printf("Invalid user password\n");
+        return -1;
+    }
+    if(flag){
+        while(1){
+        printf("------------------\n");
+        printf("1: check balance\n");
+        printf("2: deposit money\n");
+        printf("3: withdraw money\n");
+        printf("4: Finished banking\n");
+
+        printf("Please enter a value from above:");
+        scanf("%d",&choose_value);
+        if(choose_value==1){
+            printf("The current account balance is %d\n",money_value);
+        }
+        if(choose_value==2){
+            printf("Enter a deposit money amount:");
+            scanf("%d",&amount_deposited);
+            money_value=money_value+amount_deposited;
+            printf("Current account balance is %d\n",money_value);
+        }
+        if(choose_value==3){
+            printf("Enter a withdrawal money amount:");
+            scanf("%d",&amount_withdrawn);
+            money_value=money_value-amount_withdrawn;
+            printf("Current account balance is %d\n",money_value);
+        }
+        if(choose_value==4){
+            printf("Thank you have nice day\n");
+            break;
+        }
+        }
+    }
+
+    return 0;
+}
